c6dde5e7bf08f9db85aebc79698007e2fd3cbbc1
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2021-01-05 18:51:45 -0500

    api project

--- MVCAJAX/Controllers/StudentController.cs
+++ MVCAJAX/Controllers/StudentController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Domain;
-using Service;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
 using MvcAjax.Models;
 
@@ -10,64 +6,28 @@ namespace MvcAjax.Controllers
 {
     public class StudentController : Controller
     {
-        private StudentService service = new StudentService();
-
-        // GET: Student
-
+        Proxy.StudentProxy proxy = new Proxy.StudentProxy();
         public ActionResult IndexRazor()
         {
-            var model = (from c in service.Get()
-                         select new StudentModel
-                         {
-                             ID = c.studentID,
-                             Address = c.studentAddress,
-                             Code = c.studentCode,
-                             LastName = c.studentLastName,
-                             Name = c.studentName
-                         }
-                         ).ToList();
-
-            return View(model);
-        }
-
-        public ActionResult Index()
-        {
-            return View();
+            var response = Task.Run(() => proxy.GetStudentAsync());
+            return View(response.Result.listado);
         }
 
         public JsonResult getStudent(string id)
         {
-            return Json(service.Get(), JsonRequestBehavior.AllowGet);
+            var response = Task.Run(() => proxy.GetStudentAsync());
+            return Json(response.Result.listado, JsonRequestBehavior.AllowGet);
         }
-
-        public JsonResult getStudentById(int id)
-        {
-            return Json(service.GetById(id), JsonRequestBehavior.AllowGet);
-        }
-
         [HttpPost]
-        public JsonResult createStudent(Student std)
+        public ActionResult createStudent(StudentModel std)
         {
-            service.Insert(std);
-            string message = "SUCCESS";
-            return Json(new{ Message = message, JsonRequestBehavior.AllowGet });
-        }
-
-        [HttpPost]
-        public JsonResult updateStudent(Student std, int id)
-        {
-            service.Update(std, id);
-            string message = "SUCCESS";
+            var response = Task.Run(() => proxy.InsertAsync(std));
+            string message = response.Result.Mensaje;
             return Json(new { Message = message, JsonRequestBehavior.AllowGet });
         }
-
-        [HttpPost]
-        public JsonResult deleteStudent(int id)
+        public ActionResult Index()
         {
-            service.Delete(id);
-            string message = "SUCCESS";
-            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+            return View();
         }
-
     }
 }
